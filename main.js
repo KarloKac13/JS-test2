2666754eadba129ddb0d6a6ed33a35962ec1229d
KarloKac13 <karlomanojlovic@gmail.com>
2022-08-28 14:32:22 +0200

    Refactored code + added kids in tallest people fn

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 "use strict";
 
-const kids = [];
+const people = {
+  kids: [],
+  students: [],
+  workers: []
+};
 
-const workers = [];
+const properties = {
+  hairColors: ["brown", "blonde", "black"],
+  jobPositions: ["worker", "manager", "director"],
+  firms: ["firma1", "firma2", "firma3"],
+  addresses: ["adresa 123", "ulica pere perica 77", "trg zrtava programiranja 2",
+  ],
+  faculties: ["fakultet1", "fakultet2", "fakultet3"],
+};
 
-const students = [];
+const selectOptions = {
+  numberOption: [1, 2, 3, 4, 5],
+  peopleOption: ["Kids", "Students", "Workers"],
+};
 
-const hairColors = ["brown", "blonde", "black"];
-
-const jobPositions = ["worker", "manager", "director"];
-
-const firms = ["firma1", "firma2", "firma3"];
-
-const addresses = ["adresa 123", "ulica pere perica 77", "trg zrtava programiranja 2",
-];
-const faculties = ["fakultet1", "fakultet2", "fakultet3"];
-
-const arr = [1, 2, 3, 4, 5];
-const people = ["Kids", "Students", "Workers"];
-
-for (let i in arr) {
-  let option = "<option value='" + arr[i] + "'>" + arr[i] + "</option>";
+for (let i in selectOptions.numberOption) {
+  let option =
+    "<option value='" +
+    selectOptions.numberOption[i] +
+    "'>" +
+    selectOptions.numberOption[i] +
+    "</option>";
   document.getElementById("number").innerHTML += option;
 }
 
-for (let i in people) {
-  let option = "<option value='" + people[i] + "'>" + people[i] + "</option>";
+for (let i in selectOptions.peopleOption) {
+  let option =
+    "<option value='" +
+    selectOptions.peopleOption[i] +
+    "'>" +
+    selectOptions.peopleOption[i] +
+    "</option>";
   document.getElementById("people").innerHTML += option;
 }
 
@@ -35,48 +46,48 @@ let selectedPeople = document.getElementById("people");
 function selectedFn() {
   const height = Math.floor(Math.random() * 51 + 150);
   const weight = Math.floor(Math.random() * 101 + 50);
-  const hairIdx = Math.floor(Math.random() * hairColors.length);
-  const jobIdx = Math.floor(Math.random() * jobPositions.length);
-  const firmIdx = Math.floor(Math.random() * firms.length);
-  const addressIdx = Math.floor(Math.random() * addresses.length);
-  const facultiesIdx = Math.floor(Math.random() * faculties.length);
+  const hairIdx = Math.floor(Math.random() * properties.hairColors.length);
+  const jobIdx = Math.floor(Math.random() * properties.jobPositions.length);
+  const firmIdx = Math.floor(Math.random() * properties.firms.length);
+  const addressIdx = Math.floor(Math.random() * properties.addresses.length);
+  const facultiesIdx = Math.floor(Math.random() * properties.faculties.length);
 
   // Znam da je radi primjera samo, ali kids mi baš ne može
   // imati visinu i debljinu baš po jednakim parametrima kao student i adult xD
   const kidsHeight = Math.floor(Math.random() * 51 + 100);
   const kidsWeight = Math.floor(Math.random() * 51 + 50);
 
-  if (selectedPeople.value === people[0]) {
-    kids.push({
+  if (selectedPeople.value === selectOptions.peopleOption[0]) {
+    people.kids.push({
       height: kidsHeight,
       weight: kidsWeight,
-      hair: hairColors[hairIdx],
+      hair: properties.hairColors[hairIdx],
     });
   };
 
-  if (selectedPeople.value === people[1]) {
-    students.push({
+  if (selectedPeople.value === selectOptions.peopleOption[1]) {
+    people.students.push({
       height: height,
       weight: weight,
-      hair: hairColors[hairIdx],
+      hair: properties.hairColors[hairIdx],
       job: {
-        position: jobPositions[jobIdx],
-        firm: firms[firmIdx],
-        address: addresses[addressIdx],
+        position: properties.jobPositions[jobIdx],
+        firm: properties.firms[firmIdx],
+        address: properties.addresses[addressIdx],
       },
-      faculty: faculties[facultiesIdx],
+      faculty: properties.faculties[facultiesIdx],
     });
   };
 
-  if (selectedPeople.value === people[2]) {
-    workers.push({
+  if (selectedPeople.value === selectOptions.peopleOption[2]) {
+    people.workers.push({
       height: height,
       weight: weight,
-      hair: hairColors[hairIdx],
+      hair: properties.hairColors[hairIdx],
       job: {
-        position: jobPositions[jobIdx],
-        firm: firms[firmIdx],
-        address: addresses[addressIdx],
+        position: properties.jobPositions[jobIdx],
+        firm: properties.firms[firmIdx],
+        address: properties.addresses[addressIdx],
       },
     });
   };
@@ -86,116 +97,93 @@ function generatePeople() {
   for (let i = 0; i < parseInt(selectedNumber.value); i++) {
     selectedFn();
   };
-  console.log(kids, students, workers);
+  console.log(people.kids, people.students, people.workers);
 };
 
 function sortPeopleByHeight() {
-  if (selectedPeople.value === people[0]) {
-    kids.sort(function (a, b) {
-      return b.height - a.height;
-    });
-    return alert(JSON.stringify(kids));
+  if (selectedPeople.value === selectOptions.peopleOption[0]) {
+    people.kids.sort((a, b) => b.height - a.height);
+    return alert(JSON.stringify(people.kids));
   };
-  if (selectedPeople.value === people[1]) {
-    students.sort(function (a, b) {
-      return b.height - a.height;
-    });
-    return alert(JSON.stringify(students));
+  if (selectedPeople.value === selectOptions.peopleOption[1]) {
+    people.students.sort((a, b) => b.height - a.height);
+    return alert(JSON.stringify(people.students));
   };
-  if (selectedPeople.value === people[2]) {
-    workers.sort(function (a, b) {
-      return b.height - a.height;
-    });
-    return alert(JSON.stringify(workers));
+  if (selectedPeople.value === selectOptions.peopleOption[2]) {
+    people.workers.sort((a, b) => b.height - a.height
+    );
+    return alert(JSON.stringify(people.workers));
   };
 };
 
 function filterPeopleByBlondeHair() {
-  const kidsWithBlondehair = kids.filter(function (hair) {
-    return hair.hair === hairColors[1];
-  });
-  const studentsWithBlondehair = students.filter(function (hair) {
-    return hair.hair === hairColors[1];
-  });
-  const workersWithBlondehair = workers.filter(function (hair) {
-    return hair.hair === hairColors[1];
-  });
+  const kidsBlondehair = people.kids.filter((hair) => hair.hair === properties.hairColors[1]);
+  const studentsBlondehair = people.students.filter((hair) => hair.hair === properties.hairColors[1]);
+  const workersBlondehair = people.workers.filter((hair) => hair.hair === properties.hairColors[1]);
 
   alert(
     " Kids array has: " +
-      kidsWithBlondehair.length +
+      kidsBlondehair.length +
       ", Students array has: " +
-      studentsWithBlondehair.length +
+      studentsBlondehair.length +
       ", Workers array has: " +
-      workersWithBlondehair.length +
+      workersBlondehair.length +
       "\r\n" +
       " In all arrays are: " +
-      (kidsWithBlondehair.length +
-        studentsWithBlondehair.length +
-        workersWithBlondehair.length)
+      (kidsBlondehair.length +
+        studentsBlondehair.length +
+        workersBlondehair.length)
   );
-}
+};
 
 function filterPeopleByWorkplace() {
-  const studentsWorkplace = students.filter(function (work) {
-    return work.job.firm === firms[1];
-  });
-  const workersWorkplace = workers.filter(function (work) {
-    return work.job.firm === firms[1];
-  });
+  const peopleInFirma2 = [
+    people.students.filter((work) => work.job.firm === properties.firms[1]),
+    people.workers.filter((work) => work.job.firm === properties.firms[1]),
+  ];
 
-  let jsonAlertStudent = JSON.stringify(studentsWorkplace);
-  let jsonAlertWorker = JSON.stringify(workersWorkplace);
+  const jsonAlert = JSON.stringify(peopleInFirma2);
 
-  if (selectedPeople.value === people[0]) {
+  if (selectedPeople.value === selectOptions.peopleOption[0]) {
     alert("Kids don't have a job!");
-    !jsonAlertStudent;
-    !jsonAlertWorker;
+    !jsonAlert;
   } else {
-    alert(jsonAlertStudent + jsonAlertWorker);
+    alert(jsonAlert);
   };
   
 };
 
-var tallestStudentIdx = null;
-var tallestWorkerIdx = null;
-var array;
-var tallestPersonArray = [];
+const tallestPersonArray = [];
 
-function tallestPerson() {
-  if (students === []) {
-    tallestStudentIdx = null;
-    tallestWorkerIdx = workers[0].height;
-  };
+function tallestPersonFn() {
 
-  if (workers === []) {
-    tallestWorkerIdx = null;
-    tallestStudentIdx = students[0].height;
-  }
+  let tallestPerson = null;
 
-    for (var i of students) {
-      if (tallestStudentIdx <= i.height) {
-        tallestStudentIdx = i.height;
-        if (tallestStudentIdx === i.height) {
-          array = i;
-          tallestPersonArray.push(array);
-        }
-      }
-    };
-
-  for (var i of workers) {
-    if (tallestWorkerIdx <= i.height) {
-      tallestWorkerIdx = i.height;
-      if (tallestWorkerIdx === i.height) {
-        array = i;
-        tallestPersonArray.push(array)
-      };
+  for (let i of people.kids) {
+    tallestPerson = people.kids[0].height;
+    if (tallestPerson <= i.height) {
+      tallestPerson = i;
+      tallestPersonArray.push(tallestPerson);
     };
   };
 
-  tallestPersonArray.sort(function (a, b) {
-    return b.height - a.height;
-  });
+  for (let i of people.students) {
+    tallestPerson = people.students[0].height;
+    if (tallestPerson <= i.height) {
+      tallestPerson = i;
+      tallestPersonArray.push(tallestPerson);
+    };
+  };
+
+  for (let i of people.workers) {
+    tallestPerson = people.workers[0].height;
+    if (tallestPerson <= i.height) {
+      tallestPerson = i;
+      tallestPersonArray.push(tallestPerson);
+    };
+  };
+
+  tallestPersonArray.sort((a, b) => b.height - a.height);
 
   alert(JSON.stringify(tallestPersonArray[0]));
 };
@@ -204,10 +192,7 @@ const allPeopleArr = [];
 const jsonStringArr = [];
 
 function heightGreaterThanWeight() {
-  allPeopleArr.push(kids);
-  allPeopleArr.push(students);
-  allPeopleArr.push(workers);
-  console.log(allPeopleArr);
+  allPeopleArr.push(people.kids, people.students, people.workers);
   for (let i of allPeopleArr) {
     for (let j of i) {
       const value = j.height - j.weight;
@@ -220,15 +205,11 @@ function heightGreaterThanWeight() {
 };
 
 function peopleWithoutBlondeHair() {
-  const kidsWithoutBlondehair = kids.filter(function (hair) {
-    return hair.hair !== hairColors[1];
-  });
-  const studentsWithoutBlondehair = students.filter(function (hair) {
-    return hair.hair !== hairColors[1];
-  });
-  const workersWithoutBlondehair = workers.filter(function (hair) {
-    return hair.hair !== hairColors[1];
-  });
-  alert(JSON.stringify(kidsWithoutBlondehair) + JSON.stringify(studentsWithoutBlondehair) +
-    JSON.stringify(workersWithoutBlondehair));
+  const peopleWithoutBlondeHair = [
+    people.kids.filter((hair) => hair.hair !== properties.hairColors[1]),
+    people.students.filter((hair) => hair.hair !== properties.hairColors[1]),
+    people.workers.filter((hair) => hair.hair !== properties.hairColors[1]),
+  ];
+
+  alert(JSON.stringify(peopleWithoutBlondeHair));
 };
